ddc6ca73c5772bf8d067ce2bf000dd6f6805c2d5
Ajay GV <40781095+PHANIAJAY@users.noreply.github.com>
2023-11-13 13:31:41 +0530

    client.java
    when to use factory method ?
    The factory design pattern is used when we have a superclass with multiple sub-classes and based on input, we need to return one of the sub-class.

--- designPatternFactory/client.java
+++ designPatternFactory/client.java
@@ -15,6 +15,7 @@ public class client
         DropDown dropDown=uiFactory.createDropDown();
         button.sayHi();
 
+      //  The factory design pattern is used when we have a superclass with multiple sub-classes and based on input, we need to return one of the sub-class.
 
     }
 }
